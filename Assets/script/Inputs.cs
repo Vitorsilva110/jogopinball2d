112367f6d2ff41241f81a2285fc86306adb438e8
João Vitor Silva de Oliveira <20231170150037@ifrn.local>
2025-05-21 17:49:41 -0300

    adicionando efeitos

--- Assets/script/Inputs.cs
+++ Assets/script/Inputs.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 
-public class Inputs : MonoBehaviour
-
+public class InputsController : MonoBehaviour
 {
-       void Start()
+    [Header("Transform")]
+    public Transform lancador;
+
+    [Header("Hinges Joints")]
+    public HingeJoint2D hjLeft;
+    public HingeJoint2D hjRight;
+
+    [Header("Spring Joint")]
+    public SpringJoint2D spOfLancador;
+
+    void Start()
     {
-        
+        lancador = GetComponent<Transform>();
+        hjLeft = GetComponent<HingeJoint2D>();
+        hjRight = GetComponent<HingeJoint2D>();
+        spOfLancador = GetComponent<SpringJoint2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleInputs();
+    }
+
+    void HandleInputs()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            
+        }
     }
 }
